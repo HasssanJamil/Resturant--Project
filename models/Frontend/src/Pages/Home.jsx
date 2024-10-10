973eda80d5a0550103e3de207da3f162c8f1469e
Hassan Jamil <Syedhassanjamil5@gmail.com>
2024-10-10 03:17:47 -0700

    Second  commit

---
+++ models/Frontend/src/Pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import HeroSection from '../compenent/HeroSection'
+import About from '../compenent/About'
+import Qualities from '../compenent/Qualities'
+import Menu from '../compenent/Menu'
+import WhoAreWe from '../compenent/WhoAreWe'
+import Team from '../compenent/Team'
+import Reservation from '../compenent/Reservation'
+import Footer from '../compenent/Footer'
+
+
+
+
+const Home = () => {
+  return (
+    <>
+    <HeroSection/>
+   <About/>
+   <Qualities/>
+   <Menu/>
+   <WhoAreWe/>
+   <Team/>
+  <Reservation/>
+   <Footer/>
+    </>
+   
+   
+  )
+}
+
+export default Home
